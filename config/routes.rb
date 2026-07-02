55c6c31f07967b80e0e17f2e653cefef9927a2ea
Theingi Lin <102965090+theingi96@users.noreply.github.com>
2026-07-02 22:32:13 +0900

    Create page controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'resumes/new'
-  get 'resumes/create'
   devise_for :users
   root "home#index"
+  
+  get "about", to: "about#index"
+  get "skills", to: "skills#index"
+  get "projects", to: "projects#index"
+  get "experience", to: "experiences#index"
+  get "contact", to: "contacts#index"
   resources :resumes, only: [:new, :create]
 end
